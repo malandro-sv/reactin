c5b208618cc9fd7f85f62dc538ad55d6388b0b88
nam-lah <nam.lah.khan@gmail.com>
2020-07-04 10:02:42 -0600

    add Hello component

--- src/original_index.js
+++ src/original_index.js
@@ -1,15 +1,12 @@
-import 'tachyons';
-import './index.css';
-
 import React from 'react';
-import Hello from './Hello';
-
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-	  <Hello />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
