03034cef46a17c51367b27fec04ff9389dc47261
nam-lah <nam.lah.khan@gmail.com>
2020-07-04 19:21:40 -0600

    adding Card component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+var Card = () => {
+    return (
+        <div>
+            <img src='https://robohash.org/test?200x200' alt='alt robo-pic' />
+            <div>
+                <h2>Jane Doe</h2>
+                <p>jane.doe@gmail.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -2,19 +2,16 @@ import 'tachyons';
 import './index.css';
 
 import React from 'react';
-import Hello from './Hello';
+import Card from './Card';
 
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-	  <Hello />
+	  <Card />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
