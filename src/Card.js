261806b0b9aae44459e67face5391bb3c7bfe121
nam-lah <nam.lah.khan@gmail.com>
2020-07-11 21:17:54 -0600

    adding more robots for card

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-var Card = () => {
+var Card = (props) => {
     return (
-        <div>
+        <div className='bg-light-green dib br3 pa3 ma3 grow bw2 shadow-5'>
             <img src='https://robohash.org/test?200x200' alt='alt robo-pic' />
             <div>
-                <h2>Jane Doe</h2>
-                <p>jane.doe@gmail.com</p>
+                <h2>{props.name}</h2> {/* using {} */}
+                <p>{props.email}</p> // as we're using js obj
             </div>
         </div>
     );
