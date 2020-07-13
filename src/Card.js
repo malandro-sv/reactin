811a8c14d88c2cb63e72970a05fcf3f3d97069b1
nam-lah <nam.lah.khan@gmail.com>
2020-07-13 11:59:30 -0600

    destructed index.js

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 var Card = (props) => {
+
+	{/* the following line uses "destructuring"
+	to avoid using "props" in src, <h2> and <p> */}
+	var	{name, email, id} = props;
+
     return (
         <div className='bg-light-green dib br3 pa3 ma3 grow bw2 shadow-5'>
-            <img src='https://robohash.org/test?200x200' alt='alt robo-pic' />
+            <img 	src={`https://robohash.org/${id}?200x200`}
+					alt='alt robo-pic' />
             <div>
-                <h2>{props.name}</h2> {/* using {} */}
-                <p>{props.email}</p> // as we're using js obj
+                <h2>{name}</h2> {/* using {} */}
+				<p>{email}</p> {/* as we're using js obj */}
             </div>
         </div>
     );
